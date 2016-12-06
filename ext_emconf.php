02be3bfc835823e11d19349c5439d35d6c62f34b
CDG47 <n8F6NHoW>
2016-12-06 13:58:57 +0100

    Suppression éléments inutiles & Mises à jour de la structure

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,8 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '7.6.2 - 8.99.99'
+			'typo3' => '7.6.2 - 8.99.99',
+			'bootstrap_package' => '7.0.2 - 8.99.99',
 		),
 		'conflicts' => 
 		array (
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,10 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
+// Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt">');
+
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/TCEFORM.txt">');
+
 
